9278c3fc164c6ed841fc8a9f0429bc555655a89d
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-05-29 15:21:35 -0300

    Update Modelando_Banco01.sql

--- Modelando_Banco01.sql
+++ Modelando_Banco01.sql
@@ -265,14 +265,74 @@ mysql> desc telefone;
 	ON C.IDCLIENTE = T.ID_CLIENTE;
 
 --- SERIA UM RELATORIO GERAL -----
-
 */
-
-
 
 
 /* QUERY 01 -  Relatorio de homens */
 
+/* Relatorio de homens */
+SELECT C.IDCLIENTE, C.NOME, C.SEXO, C.EMAIL, C.CPF,
+	 E.RUA, E.BAIRRO, E.CIDADE, E.ESTADO, 
+	 T.TIPO, T.NUMERO
+FROM CLIENTE C
+INNER JOIN ENDERECO E
+ON C.IDCLIENTE = E.ID_CLIENTE
+INNER JOIN TELEFONE T
+ON C.IDCLIENTE = T.ID_CLIENTE
+WHERE SEXO = 'M';
+
++-----------+---------+------+-------------------+-------------+--------------------+------------+----------------+--------+------+----------+
+| IDCLIENTE | NOME    | SEXO | EMAIL             | CPF         | RUA                | BAIRRO     | CIDADE         | ESTADO | TIPO | NUMERO   |
++-----------+---------+------+-------------------+-------------+--------------------+------------+----------------+--------+------+----------+
+|         1 | JOAO    | M    | JOAOA@IG.COM      | 76567587887 | RUA CAPITAO HERMES | CENTRO     | RIO DE JANEIRO | RJ     | CEL  | 87866896 |
+|         1 | JOAO    | M    | JOAOA@IG.COM      | 76567587887 | RUA CAPITAO HERMES | CENTRO     | RIO DE JANEIRO | RJ     | RES  | 99667587 |
+|         1 | JOAO    | M    | JOAOA@IG.COM      | 76567587887 | RUA CAPITAO HERMES | CENTRO     | RIO DE JANEIRO | RJ     | COM  | 66687899 |
+|         2 | CARLOS  | M    | CARLOSA@IG.COM    | 5464553466  | RUA ALFANDEGA      | ESTACIO    | RIO DE JANEIRO | RJ     | COM  | 54768899 |
+|         2 | CARLOS  | M    | CARLOSA@IG.COM    | 5464553466  | RUA ALFANDEGA      | ESTACIO    | RIO DE JANEIRO | RJ     | CEL  | 88687909 |
+|         5 | JORGE   | M    | JORGE@IG.COM      | 8756547688  | RUA URUGUAIANA     | CENTRO     | VITORIA        | ES     | CEL  | 78458743 |
+|         5 | JORGE   | M    | JORGE@IG.COM      | 8756547688  | RUA URUGUAIANA     | CENTRO     | VITORIA        | ES     | RES  | 56576876 |
+|         5 | JORGE   | M    | JORGE@IG.COM      | 8756547688  | RUA URUGUAIANA     | CENTRO     | VITORIA        | ES     | RES  | 89986668 |
+|         9 | ANDRE   | M    | ANDRE@GLOBO.COM   | 7687567     | RUA GUEDES         | CASCADURA  | B. HORIZONTE   | MG     | RES  | 68976565 |
+|         9 | ANDRE   | M    | ANDRE@GLOBO.COM   | 7687567     | RUA GUEDES         | CASCADURA  | B. HORIZONTE   | MG     | CEL  | 99656675 |
+|        11 | KARLA   | M    | KARLA@GMAIL.COM   | 545676778   | RUA VISCONDESSA    | CENTRO     | RIO DE JANEIRO | RJ     | CEL  | 33567765 |
+|        11 | KARLA   | M    | KARLA@GMAIL.COM   | 545676778   | RUA VISCONDESSA    | CENTRO     | RIO DE JANEIRO | RJ     | CEL  | 88668786 |
+|        11 | KARLA   | M    | KARLA@GMAIL.COM   | 545676778   | RUA VISCONDESSA    | CENTRO     | RIO DE JANEIRO | RJ     | COM  | 55689654 |
+|        12 | DANIELE | M    | DANIELE@GMAIL.COM | 43536789    | RUA NELSON MANDELA | COPACABANA | RIO DE JANEIRO | RJ     | COM  | 88687979 |
+|        13 | LORENA  | M    | NULL              | 774557887   | RUA ARAUJO LIMA    | CENTRO     | VITORIA        | ES     | COM  | 88965676 |
+|        16 | ANTONIO | M    | ANTONIO@UOL.COM   | 3423565     | AV CARLOS BARROSO  | JARDINS    | SAO PAULO      | SP     | COM  | 88679978 |
+|        17 | ELAINE  | M    | ELAINE@GLOBO.COM  | 32567763    | ALAMEDA SAMPAIO    | BOM RETIRO | CURITIBA       | PR     | CEL  | 99655768 |
+|        18 | CARMEM  | M    | CARMEM@IG.COM     | 787832213   | RUA DA LAPA        | LAPA       | SAO PAULO      | SP     | RES  | 89955665 |
++-----------+---------+------+-------------------+-------------+--------------------+------------+----------------+--------+------+----------+
+
+
+/*  obs os idcliente 11, 12, 13,17 E 18. sao mulheres, precisamos alterar */
+
+SELECT * FROM CLIENTE
+WHERE IDCLIENTE = 11
+OR IDCLIENTE = 12
+OR IDCLIENTE = 13
+OR IDCLIENTE = 17
+OR IDCLIENTE = 18;
+
+/* ou dessa forma*/
+
+SELECT * FROM CLIENTE CLIENTE
+WHERE IDCLIENTE IN(11,12,13,17,18);
+
+/*ALTERANDO OS DATOS*/
+UPDATE CLIENTE SET SEXO = 'F'
+WHERE IDCLIENTE IN(11,12,13,17,18);
+
+/* Relatorio de homens */
+SELECT C.IDCLIENTE, C.NOME, C.SEXO, C.EMAIL, C.CPF,
+	 E.RUA, E.BAIRRO, E.CIDADE, E.ESTADO, 
+	 T.TIPO, T.NUMERO
+FROM CLIENTE C
+INNER JOIN ENDERECO E
+ON C.IDCLIENTE = E.ID_CLIENTE
+INNER JOIN TELEFONE T
+ON C.IDCLIENTE = T.ID_CLIENTE
+WHERE SEXO = 'M';
 
 
 
