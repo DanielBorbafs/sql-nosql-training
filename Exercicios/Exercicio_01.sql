a07500b92b55e3dafe8032560fa98d22e16914a0
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-06-09 02:26:24 -0300

    Update Exercicio_01.sql

--- Exercicios/Exercicio_01.sql
+++ Exercicios/Exercicio_01.sql
@@ -10,8 +10,34 @@ Insira dois registros na tabela "Cursos".
 Selecione todos os alunos e seus respectivos cursos usando uma junção entre as tabelas "Alunos" e "Cursos".
 */
 
+
 create table alunos (
 id_aluno int primary key auto_increment,
 nome_aluno varchar(30),
 idade int 
 );
+
+insert into alunos values(null, 'Joao', 25);
+insert into alunos values(null, 'Marcelo', 20);
+insert into alunos values(null, 'Layla', 23);
+
+select * from alunos where idade > 20;
+
+update alunos
+set id_aluno = 21
+where id_aluno = 1;
+
+delete from alunos
+where id_aluno = 2;
+
+create table cursos(
+    id_curso int primary key auto_increment,
+    nome varchar(30)
+);
+
+insert into cursos values(null,'Curso Javascript');
+insert into cursos values(null,'Curso Python');
+insert into cursos values(null,'Curso SQL');
+
+select * from alunos
+inner join cursos on alunos.id_aluno = cursos.id_curso;
