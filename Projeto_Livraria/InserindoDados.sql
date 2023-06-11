03d7067599874a0a44a87c1ea8c8d158ecd83378
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-06-10 23:14:23 -0300

    Update InserindoDados.sql

--- Projeto_Livraria/InserindoDados.sql
+++ Projeto_Livraria/InserindoDados.sql
@@ -2,3 +2,8 @@
 OBS: dados ficticios apenas para testar a modelagem de dados.
 */
 
+/* 1 - Inserindo um livro no catálogo*/
+insert into livros values(1, 'A Cabana', 'George Bech', 'Suspense');
+
+/* 2 - Inserindo a quantidade do livro no catálogo no estoque */
+insert into estoque_geral values(1,20);
