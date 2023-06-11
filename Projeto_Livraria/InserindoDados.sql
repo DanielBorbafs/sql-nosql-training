0cbfad0d025b9097404c41656439a8a5e943fa7d
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-06-11 00:14:30 -0300

    Update InserindoDados.sql

--- Projeto_Livraria/InserindoDados.sql
+++ Projeto_Livraria/InserindoDados.sql
@@ -7,3 +7,14 @@ insert into livros values(1, 'A Cabana', 'George Bech', 'Suspense');
 
 /* 2 - Inserindo a quantidade do livro no catálogo no estoque */
 insert into estoque_geral values(1,20);
+
+/* 3 - inserindo um cliente da loja */
+insert into clientes values(1, 'Marcello', 'M', 23);
+
+/* 4 - inserindo um endereço para o cliente.*/
+
+insert into endereco values(1, 'ES', 'vitoria', 'São Domingos', '589', 'lima pacheco','proximo ao banco brasil');
+
+/* 5- armazenando uma compra feita pelo cliente*/
+INSERT INTO vendas VALUES (1, 1, 1, '2023-06-11', 30.50);
+
