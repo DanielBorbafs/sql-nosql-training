4dff0b4111b166bb5556c96e81cb840ce2661c66
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-06-12 23:49:35 -0300

    Update querysSolicitadas.sql

--- Projeto_Livraria/querysSolicitadas.sql
+++ Projeto_Livraria/querysSolicitadas.sql
@@ -1,10 +1,15 @@
 
-* QUERYS
-
-
+/*
+QUERY 01 - 
+Mostra o cliente que mais fez compra na livraria,
+quanto ele gastou em R$
+e também quantas compras ele ja fez 
+*/
 SELECT c.client_id, c.nome AS cliente, COUNT(*) AS ComprasFeitas, v.vendas,
 FROM vendas v
 JOIN clientes c ON v.client_id = c.client_id
 GROUP BY c.client_id, c.nome
 ORDER BY ComprasFeitas DESC;
+
+
 
