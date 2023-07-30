d40225926ab743a6a76fa80716a89e926d82922a
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-07-29 21:13:40 -0300

    Update Basico.sql

--- Estudo/Basico.sql
+++ Estudo/Basico.sql
@@ -3,10 +3,8 @@ nomeInvestimento varchar(30),
 dataInvestimento date,
 valor int(5)
 );
-
 alter table financas
 add column idFinancas int auto_increment primary key;
-
 INSERT INTO financas (nomeInvestimento, dataInvestimento, valor)
 VALUES ('Forex', '2023-05-28', 2200);
 INSERT INTO financas (nomeInvestimento, dataInvestimento, valor)
