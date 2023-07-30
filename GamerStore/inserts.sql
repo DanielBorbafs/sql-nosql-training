b324e015529707644fee9c6ad434e37d121e551e
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-07-30 12:24:31 -0300

    Create inserts.sql

---
+++ GamerStore/inserts.sql
@@ -0,0 +1,21 @@
+-- INSERINDO DADOS ----
+-- Clientes
+insert into cliente values ( 'Daniel', 'contatodaniel@ig.com', '27998788','23', 'M','ES')
+go
+insert into cliente values ( 'Caua','caua123@gmail.com', '9987887', '18', 'M', 'ES')
+go
+
+-- PRODUTOS 
+insert into estoque values ('GOW Ragnarok', 149.90, 30)
+go
+insert into estoque values ('GTA V', 120, 20)
+go
+
+-- VENDAS 
+INSERT INTO vendas (quantidadeProduto, valorTotal, idCliente, idProduto)
+VALUES (1, 1200, 1,1)
+go
+
+select * from vendas 
+
+select * from estoque
