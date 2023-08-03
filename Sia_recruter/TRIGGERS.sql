408120250443fe04ed6e1e7e9d87e1fa5024e1a7
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-08-02 22:24:35 -0300

    Update TRIGGERS.sql

--- Sia_recruter/TRIGGERS.sql
+++ Sia_recruter/TRIGGERS.sql
@@ -1,1 +1,19 @@
+CREATE TRIGGER TR_AUMENTO_SALARIAL
+ON CARGO
+AFTER UPDATE
+AS
+BEGIN
+    IF UPDATE(SALARIO)
+    BEGIN
+        DECLARE @DataAumento DATE, @SalarioAnterior MONEY, @SalarioNovo MONEY;
 
+        SELECT @DataAumento = GETDATE();
+        SELECT @SalarioAnterior = SALARIO FROM deleted;
+        SELECT @SalarioNovo = SALARIO FROM inserted;
+
+        INSERT INTO HISTORICO_SALARIAL (IDCARGO, DATA_AUMENTO, SALARIO_ANTERIOR, SALARIO_NOVO)
+        SELECT IDCARGO, @DataAumento, @SalarioAnterior, @SalarioNovo
+        FROM inserted;
+    END;
+END;
+GO
