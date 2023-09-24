6bfd37ddfed55e72a30a42814c6b415590809a8a
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-09-24 14:30:16 -0300

    Delete ecommerce/inserts.sql

--- ecommerce/inserts.sql
+++
@@ -1,9 +0,0 @@
-/* Insert na tabela endereço */
-INSERT INTO endereco VALUES ('04567-890', 'Rua Paulista', 123, 'SP');
-INSERT INTO endereco VALUES ('30123-456', 'Avenida Afonso Pena', 789, 'MG');
-INSERT INTO endereco VALUES ('20040-010', 'Avenida Rio Branco', 456, 'RJ');
-INSERT INTO endereco VALUES ('80020-060', 'Rua XV de Novembro', 987, 'PR');
-INSERT INTO endereco VALUES ('40010-010', 'Largo do Pelourinho', 321, 'BA');
-go
-
-
