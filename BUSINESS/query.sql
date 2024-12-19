0e2063d1ad3ab9c371b6bc51e79a80c86f5fa590
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-12-19 10:55:52 -0300

    Update query.sql

--- BUSINESS/query.sql
+++ BUSINESS/query.sql
@@ -14,3 +14,23 @@ SELECT PROFISSAO, SUM(SALARIO) AS TOTAL_SALARIOS
 FROM FUNCIONARIOS
 GROUP BY PROFISSAO
 ORDER BY TOTAL_SALARIOS DESC; -- Ordena do maior para o menor total de salários
+
+
+/* 
+Precisamos extrair a contagem de gênero cadastrados em nosso sistema:
+- Verificar se possuí dados nulos;
+- Fazer a contagem
+*/
+UPDATE BUSINESS
+SET GENERO = 'N' -- NÃO INFORMADO
+WHERE GENERO IS NULL
+
+SELECT GENERO, COUNT(GENERO) AS [CONTAGEM DE SEXO]
+FROM BUSINESS
+GROUP BY GENERO
+GO
+/*
+N	113 - NÃO INFORMADO
+F	455 - FEMININO
+M	432 - MASCULINO
+*/
